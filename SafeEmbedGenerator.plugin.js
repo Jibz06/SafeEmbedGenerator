68e9d7f5b81fb282ebbc568f10fba088e7445368
Kyza <TylerC2077@gmail.com>
2019-04-24 19:52:58 -0400

    Updated plugin

--- SafeEmbedGenerator.plugin.js
+++ SafeEmbedGenerator.plugin.js
@@ -95,7 +95,6 @@ function addButton() {
     var permissions = channel.discordObject.permissions;
 
     // Only add the button if the user has permissions to send messages and embed links.
-		console.log(isAllowed());
     if (isAllowed() && (hasPermission("textEmbedLinks") && hasPermission("textSendMessages")) || channel.type != "GUILD_TEXT") {
       if (document.getElementsByClassName("embed-button-wrapper").length == 0) {
         var daButtons = document.getElementsByClassName("da-buttons")[0];
